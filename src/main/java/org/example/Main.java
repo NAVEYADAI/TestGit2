69b8d7a8ec8b50fb58a7c42d77dfcaf4b10de48d
NAVE YADAI <87646702+NAVEYADAI@users.noreply.github.com>
2023-01-15 18:14:03 +0300

    3 example

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,12 +1,16 @@
 package org.example;
-/*
+/**
    owner : nave yadai
+
+   @author naveyadai
    id    : 326296472
  */
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-       System.out.println("ORIMERHAVI");
+        System.out.println("ORIMERHAVI");
         int f=0;
+        int myAddString = 67;
+
     }
 }

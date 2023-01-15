00947b0304f6c29315b3bf471e69a356e8804fcb
NAVE YADAI <87646702+NAVEYADAI@users.noreply.github.com>
2023-01-15 16:05:41 +0300

    add a name and id for main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,8 @@
 package org.example;
-
+/*
+   owner : nave yadai
+   id    : 326296472
+ */
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
